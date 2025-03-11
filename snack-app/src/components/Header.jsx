0de90476271ede783895071eaf8f0614c27d4b62
quantanmreaper <138207080+quantanmreaper@users.noreply.github.com>
2025-03-11 21:36:26 +0300

    Header Modification

--- snack-app/src/components/Header.jsx
+++ snack-app/src/components/Header.jsx
@@ -7,7 +7,7 @@ const header = () =>{
               <nav className="max-w-7xl mx-auto px-6 sm:px-8 lg:px-10">
                 <div className="flex justify-between items-center h-20">
 
-                  <div className="flex-shrink-0">
+                  <div className="flex-shrink-0 ml-[-80px] ">
                     <a href="/" className="text-2xl font-bold trackling-wide">Snackify</a>
                   </div>
 
@@ -25,7 +25,7 @@ const header = () =>{
                   </div>
  
                   <div className="md:hidden">
-                    <button type="button" className="bg-white inline-flex items-center justify-center p-3 rounded-md text-gray-600 hover:text-yellow-400 hover:bg-yellow-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-300">
+                    <button type="button" className="bg-yellow-500 hover:bg-yellow-100 inline-flex items-center justify-center p-3 rounded-md text-gray-900 hover:text-yellow-400 hover:bg-black-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-300">
                       <span className="sr-only">Open main menu</span>
                         <svg className="block h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
                           <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16" />
